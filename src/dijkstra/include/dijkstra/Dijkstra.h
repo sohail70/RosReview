4090a14d5580588aabf1b2549a31b16c27478039
sohail70 <sohail.e.nia@gmail.com>
2022-11-27 17:23:50 +0330

    Add dijkstra library structure and a test file to test the lib

---
+++ src/dijkstra/include/dijkstra/Dijkstra.h
@@ -0,0 +1,36 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+
+#include<ros/ros.h>
+#include<costmap_2d/costmap_2d_ros.h>
+#include<costmap_2d/costmap_2d.h>
+#include<nav_core/base_global_planner.h>
+#include<geometry_msgs/PoseStamped.h>
+#include<tf/tf.h>
+
+
+namespace global_planner{
+    class Dijkstra: public nav_core::BaseGlobalPlanner
+    {
+        public:
+
+            Dijkstra();
+            Dijkstra(std::string name_ , costmap_2d::Costmap2DROS* costmap_ros);
+
+
+            void initialize(std::string name_, costmap_2d::Costmap2DROS* costmap_ros) override;
+
+            bool makePlan(const geometry_msgs::PoseStamped& start, 
+            const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan) override;
+
+        private:
+            std::string name;
+            costmap_2d::Costmap2DROS* costmap;
+            
+    };
+
+
+};
+
+#endif
