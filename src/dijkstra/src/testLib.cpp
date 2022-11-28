0eeaf63e045db654f19e879a901318892682caaf
sohail70 <sohail.e.nia@gmail.com>
2022-11-28 15:12:20 +0330

    Add test for path planning part-1

--- src/dijkstra/src/testLib.cpp
+++ src/dijkstra/src/testLib.cpp
@@ -1,7 +1,22 @@
 #include<Dijkstra.h>
 
+#include<nav_msgs/OccupancyGrid.h>
 
-int main()
+// for ros info https://answers.ros.org/question/233107/why-doesnt-ros_info-allow-calling-stdqueuesize-inside-as-a-param/
+void costmap_callback(const nav_msgs::OccupancyGrid::ConstPtr& data)
 {
+    ROS_INFO("MAP SIZE: %zd",data->data.size());
+}
 
+
+int main(int argc , char** argv)
+{
+    ros::init(argc,argv,"test");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe<nav_msgs::OccupancyGrid>("/move_base/global_costmap/costmap",1,&costmap_callback);
+
+    ros::spin();
+
+    return 0;
 }
