8509a704d34b19015cfba154b2efbf63ed1c78a3
sohail70 <sohail.e.nia@gmail.com>
2022-12-06 19:05:48 +0330

    Add neighborToList func and custom data type for unordered_list

--- src/dijkstra/include/dijkstra/Dijkstra.h
+++ src/dijkstra/include/dijkstra/Dijkstra.h
@@ -11,7 +11,21 @@ remember to create a utility class for visualization
 
 */
 
+/*
+    getCost function returns these numbers
 
+    255 ---> unknown
+    Pink Color ---> less than 254(i think)
+    Red color ---> less than Pink
+    Blue ---> less than red
+    Pale blue ---> less than blue
+    White ---> 0 ---> white space
+    
+    
+
+    ba dastor paeen cost haro check kun ---> khoob mishod age probability ro migereftim yani -1,[0,100] ke dar costmap migereftim
+    ROS_ERROR("COOOOOST %d" , costmap_ros->getCostmap()->getCost(25,20));
+*/
 
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
@@ -24,6 +38,9 @@ remember to create a utility class for visualization
 #include<geometry_msgs/PoseStamped.h>
 #include<tf/tf.h>
 #include<queue>
+#include<unordered_set>
+#include<algorithm>
+#include<numeric>
 
 
 #include"PointWrapper.h"
@@ -33,14 +50,32 @@ namespace global_planner{
 
     struct Vertex{
         float g_value;
-        int cell_x;
-        int cell_y;
+        unsigned int cell_x;
+        unsigned int cell_y;
+        Vertex* parent;
+
+        bool operator==(const Vertex& other) const //const ro nazari static_assert error mide
+        {
+            return g_value == other.g_value;
+        }
+
+        bool operator!=(const Vertex& other) const
+        {
+            return (cell_x != other.cell_x && cell_y != other.cell_y);
+        }
     };
 
     struct Order{
         bool operator()(Vertex a , Vertex b)
         {
             return a.g_value > b.g_value;
+        }
+    };
+
+    struct MyHash{
+        std::size_t operator()(Vertex v) const{
+            std::hash<int> hashVal;
+            return hashVal(v.g_value);
         }
     };
 
@@ -59,13 +94,13 @@ namespace global_planner{
             const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan) override;
 
             void show();
-
+            void neighborsToList(Vertex current_node);
         private:
             std::string name;
             costmap_2d::Costmap2DROS* costmap_ros;//in va paeeni ye functionality daran faghat paeeni ke man khodam neveshtamesh pose ro ham mide vali be nazar niazi nabode ke nanveshtan chun dar global planner robot ke harkat nemikune ke modhem bashe ghazie
             // CostmapUtility cUtil; // be inniazi nist chun worldtomap va func e maptoworld dar costmap2dROS inkaro baramoon mikune
             std::priority_queue<Vertex , std::vector<Vertex> , Order> open_list;
-            
+            std::unordered_set<Vertex,MyHash> closed_list; 
 
             PointWrapper* points;
             
--- src/dijkstra/src/Dijkstra.cpp
+++ src/dijkstra/src/Dijkstra.cpp
@@ -45,47 +45,69 @@ namespace global_planner{
         // plan.push_back(goal);
         // return true;
     /*******************************Testing the costmap 2d ros functions*****************************************/
-        ROS_INFO("myDIJKSTRA planning a route from start pose to goal pose ... ");
-        // ROS_ERROR("EROOOR %s" ,costmap->getName().c_str());
-        geometry_msgs::PoseStamped robotPose;
-        costmap_ros->getRobotPose(robotPose);  //ino dar cmake yadet nare be library myDijkstra ---> /opt/ros/noetic/lib/libcostmap_2d.so --> alabte vase func e getname niazi be in nist chun functionality ye seri ha dar header tarif shode 
-        auto costmap  = costmap_ros->getCostmap();
-        ROS_ERROR("posex: %f posey: %f",robotPose.pose.position.x , robotPose.pose.position.y);
+        // ROS_INFO("myDIJKSTRA planning a route from start pose to goal pose ... ");
+        // // ROS_ERROR("EROOOR %s" ,costmap->getName().c_str());
+        // geometry_msgs::PoseStamped robotPose;
+        // costmap_ros->getRobotPose(robotPose);  //ino dar cmake yadet nare be library myDijkstra ---> /opt/ros/noetic/lib/libcostmap_2d.so --> alabte vase func e getname niazi be in nist chun functionality ye seri ha dar header tarif shode 
+        // auto costmap  = costmap_ros->getCostmap();
+        // ROS_ERROR("posex: %f posey: %f",robotPose.pose.position.x , robotPose.pose.position.y);
 
-        unsigned int cell_x , cell_y;
-        costmap->indexToCells(511 , cell_x , cell_y); //0--255   256---511   512---(512+256)     a---(a+map.width)
-        ROS_ERROR("cell_x: %i  cell_y: %i" , cell_x , cell_y);
+        // unsigned int cell_x , cell_y;
+        // costmap->indexToCells(511 , cell_x , cell_y); //0--255   256---511   512---(512+256)     a---(a+map.width)
+        // ROS_ERROR("cell_x: %i  cell_y: %i" , cell_x , cell_y);
 
-        // ROS_ERROR("POSE X: %f POSE Y: %f",cUtil.cell_to_pose(511).x , cUtil.cell_to_pose(511).y);
+        // // ROS_ERROR("POSE X: %f POSE Y: %f",cUtil.cell_to_pose(511).x , cUtil.cell_to_pose(511).y);
 
-        costmap->worldToMap(robotPose.pose.position.x , robotPose.pose.position.y ,cell_x , cell_y );
-        ROS_ERROR("Start cell x :  %i Start cell y: %i" , cell_x , cell_y);
+        // costmap->worldToMap(robotPose.pose.position.x , robotPose.pose.position.y ,cell_x , cell_y );
+        // ROS_ERROR("Start cell x :  %i Start cell y: %i" , cell_x , cell_y);
 
 
-        std_msgs::ColorRGBA color; color.a = 1; color.g = 1;
+        // std_msgs::ColorRGBA color; color.a = 1; color.g = 1;
         
-        ros::Rate loop_rate(1);
+        // ros::Rate loop_rate(1);
 
-        points = new PointWrapper(color,"map");
-        while(ros::ok())
-        {
+        // points = new PointWrapper(color,"map");
+        // while(ros::ok())
+        // {
             
-            costmap_ros->getRobotPose(robotPose);
-            costmap->worldToMap(robotPose.pose.position.x , robotPose.pose.position.y ,cell_x , cell_y );
-            geometry_msgs::Point p;
-            costmap->mapToWorld(cell_x , cell_y , p.x , p.y);
-            points->addPoint(p);
-            points->publish();
-            ROS_ERROR("PUBLISHING POINTS");
-            ros::spinOnce();
-            loop_rate.sleep();
+        //     costmap_ros->getRobotPose(robotPose);
+        //     costmap->worldToMap(robotPose.pose.position.x , robotPose.pose.position.y ,cell_x , cell_y );
+        //     geometry_msgs::Point p;
+        //     costmap->mapToWorld(cell_x , cell_y , p.x , p.y);
+        //     points->addPoint(p);
+        //     points->publish();
+        //     ROS_ERROR("PUBLISHING POINTS");
+        //     ros::spinOnce();
+        //     loop_rate.sleep();
 
-        }
+        // }
 
-        delete points;
+        // delete points;
 
         
-    /*************************************************************************************************************/
+    /************************************Dijkstra implementation*************************************************************************/
+        
+        Vertex goal_node;
+        Vertex start_node; start_node.g_value = 0;
+        costmap_ros->getCostmap()->worldToMap(goal.pose.position.x , goal.pose.position.y ,goal_node.cell_x ,goal_node.cell_y ); 
+        costmap_ros->getCostmap()->worldToMap(start.pose.position.x , start.pose.position.y ,start_node.cell_x , start_node.cell_y);
+
+
+        open_list.push(start_node);
+        Vertex current_node;
+        std::vector<Vertex> current_neighbors;        
+
+        ros::Rate loop_rate(1);
+        while(ros::ok() || current_node != goal_node)
+        {
+            current_node  = open_list.top();
+            neighborsToList(current_node);
+            open_list.pop();
+            closed_list.insert(current_node);
+            
+        }         
+
+        
         
         return true;
     }
@@ -94,6 +116,33 @@ namespace global_planner{
         ROS_INFO("Dijkstra Algorithm");
     }
 
+    void Dijkstra::neighborsToList(Vertex current_node) 
+    {
+        // right
+        if( costmap_ros->getCostmap()->getCost(current_node.cell_x + 1 , current_node.cell_y) ==costmap_2d::FREE_SPACE) // age cell_x va cell_y < 0 beshe irad dare pas badan ye check bezar bara in
+        {
+            Vertex v;v.cell_x = current_node.cell_x+1; v.cell_y = current_node.cell_y ; v.parent= &current_node; v.g_value = current_node.g_value+1;
+            open_list.push(v);
+        }
+        // up
+        if(costmap_ros->getCostmap()->getCost(current_node.cell_x , current_node.cell_y + 1) == costmap_2d::FREE_SPACE)
+        {
+            Vertex v;v.cell_x = current_node.cell_x; v.cell_y = current_node.cell_y+1 ; v.parent = &current_node; v.g_value = current_node.g_value+1;
+            open_list.push(v);
+        }
+        // left
+        if(costmap_ros->getCostmap()->getCost(current_node.cell_x - 1 , current_node.cell_y) == costmap_2d::FREE_SPACE)
+        {
+            Vertex v;v.cell_x = current_node.cell_x-1; v.cell_y = current_node.cell_y ; v.parent = &current_node; v.g_value = current_node.g_value+1;
+            open_list.push(v);
+        }
+        // down
+        if(costmap_ros->getCostmap()->getCost(current_node.cell_x  , current_node.cell_y - 1)== costmap_2d::FREE_SPACE)
+        {
+            Vertex v;v.cell_x = current_node.cell_x; v.cell_y = current_node.cell_y-1 ; v.parent = &current_node; v.g_value = current_node.g_value+1; 
+            open_list.push(v);
+        }
+    }
 
 
 }
