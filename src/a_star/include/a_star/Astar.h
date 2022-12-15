fcf43b48572cdcda316d0e8c17e06b11585846ff
sohail70 <sohail.e.nia@gmail.com>
2022-12-15 14:10:44 +0330

    Add manhattan and euclidean distance and h cost calculation

--- src/a_star/include/a_star/Astar.h
+++ src/a_star/include/a_star/Astar.h
@@ -3,7 +3,9 @@
 #include<costmap_2d/costmap_2d.h>
 #include<nav_core/base_global_planner.h>
 #include<PointWrapper.h>
-
+#include<queue>
+#include<array>
+#include<cmath>
 namespace global_planner{
 
     class Astar: public nav_core::BaseGlobalPlanner{
@@ -13,8 +15,19 @@ namespace global_planner{
             void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros); 
             bool makePlan(const geometry_msgs::PoseStamped& start, 
                                 const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
+            float hValue(unsigned int cell_index ,  unsigned int goal_index);
+            float euc(int a_cell_x , int a_cell_y, int b_cell_x , int b_cell_y);
+
+            float man(int a_cell_x , int a_cell_y, int b_cell_x , int b_cell_y);
+            
         private:
             costmap_2d::Costmap2DROS* costmap_ros;
+            // Q(g_value , index)
+            std::priority_queue<std::pair<unsigned int , unsigned int> , std::vector<std::pair<unsigned int , unsigned int>> , std::greater<std::pair<unsigned int , unsigned int>>> open_list;
+            
+
+            std::unique_ptr<PointWrapper> points;
+            std::unique_ptr<PointWrapper> points2;
 
     };
 }
--- src/a_star/src/Astar.cpp
+++ src/a_star/src/Astar.cpp
@@ -10,6 +10,11 @@ namespace global_planner{
     void Astar::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
     {
         this->costmap_ros = costmap_ros;
+        std_msgs::ColorRGBA color; color.a =1 ; color.g = 1;
+        std_msgs::ColorRGBA color1; color1.a = 1; color1.r = 1;
+        points = std::make_unique<PointWrapper>(color , "map");
+        points2 = std::make_unique<PointWrapper>(color1 , "map");
+        
     }
 
     
@@ -17,6 +22,33 @@ namespace global_planner{
     {
         ROS_ERROR("ASTAR STARTS");
 
+        unsigned int start_index , start_cell_x , start_cell_y ,goal_index ,  goal_cell_x , goal_cell_y;
+        costmap_ros->getCostmap()->worldToMap(start.pose.position.x, start.pose.position.y , start_cell_x , start_cell_y );
+        costmap_ros->getCostmap()->worldToMap(goal.pose.position.x , goal.pose.position.y , goal_cell_x , goal_cell_y);
+        costmap_ros->getCostmap()->getIndex(start_cell_x , start_cell_y);
+        costmap_ros->getCostmap()->getIndex(goal_cell_x , goal_cell_y );
+
+
         return true;
     }
+
+    float Astar::hValue(unsigned int cell_index , unsigned int goal_index)
+    {
+        unsigned int cell_x , cell_y , goal_cell_x , goal_cell_y;
+        costmap_ros->getCostmap()->indexToCells(cell_index , cell_x , cell_y);
+        costmap_ros->getCostmap()->indexToCells(goal_index , goal_cell_x , goal_cell_y);
+        return euc(cell_x , cell_y , goal_cell_x , goal_cell_y); 
+        // return man();
+    }
+
+    float euc(int a_cell_x , int a_cell_y , int b_cell_x , int b_cell_y)
+    {
+        return std::sqrt(std::pow((a_cell_x -b_cell_x) , 2) + std::pow((a_cell_y - b_cell_y),2));
+    }
+
+    float man(int a_cell_x , int a_cell_y , int b_cell_x , int b_cell_y)
+    {
+        return std::abs(a_cell_x - b_cell_x) + std::abs(a_cell_y - b_cell_y); 
+    }
+
 }
