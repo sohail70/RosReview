0ef4fe6b000a9c40483a2dd1af92a55740da0c66
sohail70 <sohail.e.nia@gmail.com>
2022-12-26 16:36:20 +0330

    Get the Costmap2DROS object from the already created global costmap
    You have to create a rosnode with the name global_costmap and an object of Costmap2DROS and pass the string "costmap" to it. This is because to match
    the topic global_costmap/costmap/costmap.

--- src/lpa_star/include/lpa_star/lpa_star.h
+++ src/lpa_star/include/lpa_star/lpa_star.h
@@ -1,9 +1,15 @@
+//https://yuzhangbit.github.io/tools/several-ways-of-writing-a-ros-node/   ---> READ IT
+//https://answers.ros.org/question/228253/generating-a-2d-costmap-from-a-mapyaml-file/  --> you don't have to read it
 #include<iostream>
 #include<costmap_2d/costmap_2d.h>
 #include<geometry_msgs/PoseStamped.h>
 #include<queue>
 #include<PointWrapper.h>
+#include<nav_msgs/OccupancyGrid.h>
+#include<costmap_2d/costmap_2d_ros.h>
 
+
+#include <tf2_ros/transform_listener.h>
 
 
 namespace DynamicPlanner{
@@ -14,10 +20,19 @@ namespace DynamicPlanner{
             ~LpaStar();
             void initialize(const costmap_2d::Costmap2D& costmap);
             bool makePlan(const geometry_msgs::PoseStamped& start , const geometry_msgs::PoseStamped& goal , std::vector<geometry_msgs::PoseStamped> plan);
-            void updatePlan(std::vector<geometry_msgs::PoseStamped>& plan);
+            void computeShortestPath();
+            void updateVertex();
+            void calculateKey();
             
         private:
-            costmap_2d::Costmap2D* costmap;
+            ros::NodeHandle nh;
+            ros::Subscriber map_sub;
+
+            costmap_2d::Costmap2D* costmap; //https://answers.ros.org/question/60026/difference-between-costmap2d-and-occupancygrid-not-clear/
+            costmap_2d::Costmap2DROS* costmap_ros;
+            nav_msgs::OccupancyGrid* map;
+
+            
             // Q(key1 , key2 , index of a node in costmap array)
             std::priority_queue<std::tuple<float, float , int> , std::vector<std::tuple<float, float , int>> ,std::greater<std::tuple<float, float , int>> > q;
 
--- src/lpa_star/src/lpa_star.cpp
+++ src/lpa_star/src/lpa_star.cpp
@@ -1,7 +1,54 @@
+//384 X 256 0.1 resolution
 #include<ros/ros.h>
 #include<lpa_star/lpa_star.h>
 
+
+namespace DynamicPlanner{
+    LpaStar::LpaStar(){
+       
+    }
+
+    void LpaStar::initialize(const costmap_2d::Costmap2D& costmap)
+    {
+
+    }
+
+
+    bool LpaStar::makePlan(const geometry_msgs::PoseStamped& start , const geometry_msgs::PoseStamped& goal , std::vector<geometry_msgs::PoseStamped> plan)
+    {
+
+    }
+
+
+}
+
+
+
+
+
+
+#include<geometry_msgs/PoseStamped.h>
+//https://answers.ros.org/question/332304/problem-with-costmap2dros-and-tf-transform-listener-in-ros-melodic/
+costmap_2d::Costmap2DROS* lcr ;
+
 int main(int argc , char** argv)
 {
-    ros::init(argc, argv , "lpa_star");
+    ros::init(argc, argv , "global_costmap");
+    ros::NodeHandle nh;
+
+    geometry_msgs::PoseStamped pose;
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tf2_listener(tfBuffer);;
+    lcr = new costmap_2d::Costmap2DROS("costmap", tfBuffer);
+    lcr->start();
+    // ROS_ERROR("ASDASD %f %f %f " , lcr->getRobotFootprint().at(0).x , lcr->getRobotFootprint().at(0).y, lcr->getRobotFootprint().at(0).z);
+    
+    while(ros::ok())
+    {
+
+        lcr->getRobotPose(pose);
+        ROS_ERROR("POSE: %f %f" , pose.pose.position.x , pose.pose.position.y);
+        ros::spinOnce();
+    }
 }
